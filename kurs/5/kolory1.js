d59528142828038066225898cf940d08d1ba755d
Kamiladach <adachkamil@gmail.com>
2018-01-13 16:30:46 +0100

    Colory 1

--- kurs/5/kolory1.js
+++ kurs/5/kolory1.js
@@ -4,6 +4,7 @@ var elZielony=document.getElementById("zielony");
 var elPomaranczowy=document.getElementById("pomaranczowy");
 var elInny=document.getElementById("inny");
 var elWynik=document.getElementById("wynik");
+var elWynikKolor=document.getElementById("wynikKolor");
 var elWybierzKolor=document.getElementById("wybierzKolor");
 elWybierzKolor.style.visibility= "hidden";
 var innyKolor;
@@ -11,19 +12,25 @@ var innyKolor;
 elPrzycisk.onclick=function () {
    // alert("test");
     if (elCzerwony.checked){
-       elWynik.innerHTML="Twoj ulubiony kolor: czerwony";
-
-
+       elWynik.innerHTML="Twoj ulubiony kolor: ";
+        elWynikKolor.innerHTML = "czerwony";
+        elWynikKolor.style.color= "red";
         }else if(elZielony.checked){
-        elWynik.innerHTML="Twoj ulubiony kolor: zielony";
+        elWynik.innerHTML="Twoj ulubiony kolor: ";
+            elWynikKolor.innerHTML = "zielony";
+             elWynikKolor.style.color= "green";
             }
 
      else if(elPomaranczowy.checked){
-        elWynik.innerHTML="Twoj ulubiony kolor: Pomaranczowy";
+        elWynik.innerHTML="Twoj ulubiony kolor: ";
+         elWynikKolor.innerHTML = "pomaranczowy";
+          elWynikKolor.style.color= "orange";
             }
         else  { //console.log(elWybierzKolor.value);
                innyKolor = elWybierzKolor.value;
-            elWynik.innerHTML="Twoj ulubiony kolor: " + innyKolor;
+            elWynik.innerHTML="Twoj ulubiony kolor: ";
+            elWynikKolor.innerHTML = innyKolor;
+            elWynikKolor.style.color= innyKolor;
               }
     }
 elInny.onclick=function(){
